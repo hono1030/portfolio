51c62feba24dc194e0e8a5818de763a1109ab3f2
hono1030 <honoka.n1030@gmail.com>
2025-02-07 14:25:47 +0900

    Update: experience-section

--- src/data/experience.ts
+++ src/data/experience.ts
@@ -1,16 +1,27 @@
 import { TFunction } from "i18next";
 import { Experience } from "../types/types";
-import findADoc_logo from "../assets/find-a-doc-logo.png";
+import sustainability_logo from "../assets/sustainability_logo.jpeg";
+// import findADoc_logo from "../assets/find-a-doc-logo.png";
 
 export const getExperience = (t: TFunction): Experience[] => [
   {
-    name: "Find a Doc, Japan",
+    name: "Sustainability Page",
     role: "Frontend Engineer",
-    description: t("experience.find-a-doc.description"),
-    logo: findADoc_logo,
-    techStack: ["TypeScript", "Vue.js", "Nuxt", "Pinia", "Node.js"],
-    site: "https://www.findadoc.jp/",
+    description: t("experience.sustainability.description"),
+    logo: sustainability_logo,
+    techStack: ["Next.js", "TypeScript", "Tailwind CSS", "Playwright"],
+    site: "https://sustainabilitypage.com/",
     github: "",
-    // github: "https://github.com/ourjapanlife/findadoc-web?tab=readme-ov-file",
+    // github: "https://github.com/sustainabilitypage/sustainability-frontend",
   },
+  //   {
+  //     name: "Find a Doc, Japan",
+  //     role: "Frontend Engineer",
+  //     description: t("experience.find-a-doc.description"),
+  //     logo: findADoc_logo,
+  //     techStack: ["TypeScript", "Vue.js", "Nuxt", "Pinia", "Node.js"],
+  //     site: "https://www.findadoc.jp/",
+  //     github: "",
+  //     // github: "https://github.com/ourjapanlife/findadoc-web?tab=readme-ov-file",
+  //   },
 ];
